2dec754f9ef1c89e8e2ad582cc7f30c66c134a6f
Martonsan <130407455+Martonsan@users.noreply.github.com>
2023-04-14 15:09:50 -0400

    loop complete

--- main.py
+++ main.py
@@ -12,3 +12,20 @@ print(restaurant_names)
 transportation_names = ['Taxi', 'Bicycle', 'Boat']
 print(transportation_names)
 
+entertainment_names = ['Belly Dancing', 'Watersking', 'Hiking']
+print(entertainment_names)
+
+for first_destination in destination_names:
+    print(first_destination)
+
+for first_retaurant in restaurant_names:
+    print(first_retaurant)
+for first_transportation in transportation_names:
+    print(first_transportation)
+for first_entertainment in entertainment_names:
+    print(first_entertainment)
+        
+
+                       
+
+
