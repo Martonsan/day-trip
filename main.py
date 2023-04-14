257f9c846d15a0ae13048fcf81d8332220875335
Martonsan <130407455+Martonsan@users.noreply.github.com>
2023-04-14 13:30:09 -0400

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print('Hello World')
+# day-trip
+Project one
+
+list_of_destinations = ["Dubai", "Florida", "Colorado"]
+print(list_of_destinations)
+
+
+
+list_of_restaurants = ['Seafood','Thai','BBQ']
+print
+
+
+modes_of_transportations = ['Taxi','Bicycle','Boat']
+print
